c9dde506f39028f9de674eb20ea77937ca70bffa
Andres Haro <haro12041968@gmail.com>
2026-05-06 16:23:07 +0200

    Fix WhatsApp outbound auth module dependency

--- apps/api/src/whatsapp/whatsapp.module.ts
+++ apps/api/src/whatsapp/whatsapp.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { WhatsappCloudService } from './whatsapp-cloud.service';
 import { WhatsappOutboundController } from './whatsapp-outbound.controller';
 
 @Module({
+  imports: [AuthModule],
   controllers: [WhatsappOutboundController],
   providers: [WhatsappCloudService],
   exports: [WhatsappCloudService],
